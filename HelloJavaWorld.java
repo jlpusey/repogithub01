c0ce7e03d11b72728d9f14d3deb85aeb7adf1d8b
jlpusey <jlpusey@cirilgroup.com>
2024-12-09 16:00:08 +0100

    Update HelloJavaWorld.java

--- HelloJavaWorld.java
+++ HelloJavaWorld.java
@@ -1,6 +1,6 @@
 public class HelloJavaWorld {
 
         public static void main(String[] args) {
-                System.out.println("Hello java world");
+                System.out.println("Hello java world test");
         }
 }
